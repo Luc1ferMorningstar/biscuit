37661055c7ee43a7aae622406b9fcefd347828b7
Yevhenii Buhor <eugene.bugor@do-it.co>
2018-05-06 21:48:42 +0300

    some fixes

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import HeaderNavbar from '../../components/Header/Navbar/HeaderNavbar';
-import dividerImage from '../../assets/images/divider-wave.png';
 import './Header.css';
 
 const Header = () => {
